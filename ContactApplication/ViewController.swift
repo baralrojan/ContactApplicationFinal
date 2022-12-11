edfc89079f87a93f29f50b0fd2fcd8968c96b136
baralrojan <31406771+baralrojan@users.noreply.github.com>
2022-12-11 15:26:47 -0500

    Initial Commit

---
+++ ContactApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactApplication
+//
+//  Created by user230806 on 12/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
